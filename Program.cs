e95a5dc6b385d53aa8073ddf224aea99fff86c77
канаевдс <@TS-06>
2021-09-10 15:27:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation Calc = new Calculation("InputData.csv", "OutputData.txt");
+            Calc.CalcStart();
+        }
+    }
+}
